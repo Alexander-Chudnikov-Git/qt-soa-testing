a4f279af102ccf0f8ae64646e207b41299a3706d
Alexander-Chudnikov-Git <admin@redline-software.xyz>
2025-03-17 00:27:20 +0300

    fix(logging)

--- src/app/main_window/main_window.cpp
+++ src/app/main_window/main_window.cpp
@@ -31,25 +31,19 @@ MainWindow::~MainWindow()
 void MainWindow::initialize()
 {
 	QRect window_rect = UTILS::SettingsManager::instance()->getValue(UTILS::SettingsManager::Setting::WINDOW_RECT).toRect();
-
 	this->setWindowFlags(Qt::Dialog | Qt::WindowStaysOnTopHint | Qt::X11BypassWindowManagerHint | Qt::FramelessWindowHint |
 						 Qt::SplashScreen | Qt::MSWindowsFixedSizeDialogHint | Qt::BypassWindowManagerHint |
 						 Qt::MSWindowsOwnDC | Qt::WindowOverridesSystemGestures);
-
 	this->move(window_rect.x(), window_rect.y());
 	this->setFixedSize(window_rect.width(), window_rect.height());
 	this->showFullScreen();
-
 	this->m_move_resize_timer->setInterval(200);
 	this->m_move_resize_timer->setSingleShot(false);
 	this->m_move_resize_timer->start();
-
 	this->setupUi();
 	this->setupConnections();
 	this->setupStyle();
-
 	qApp->installEventFilter(this);
-
 	disableAllGSettingsKeybinds();
 }
 
@@ -58,8 +52,6 @@ void MainWindow::setupUi()
 	auto layout_wrapper = new QWidget();
 	this->m_main_layout = new QGridLayout();
 	this->m_user_panel	= new UserPanelWidget();
-
-	// Debug close button
 	QPushButton *button = new QPushButton("Close Application");
 	connect(button, &QPushButton::clicked, this, [this]() {
 		restoreAllGSettingsKeybinds();
@@ -68,12 +60,9 @@ void MainWindow::setupUi()
 		qApp->quit();
 	});
 	this->m_main_layout->addWidget(button, 0, 0);
-
 	this->m_main_layout->addWidget(this->m_user_panel, 0, 1);
-
 	layout_wrapper->setLayout(this->m_main_layout);
 	this->setCentralWidget(layout_wrapper);
-
 	this->setFocus();
 	this->activateWindow();
 	this->raise();
@@ -102,7 +91,6 @@ void MainWindow::resizeEvent(QResizeEvent *event)
 void MainWindow::onMoveResizeTimerTimeout()
 {
 	QRect window_rect = UTILS::SettingsManager::instance()->getValue(UTILS::SettingsManager::Setting::WINDOW_RECT).toRect();
-
 	this->move(window_rect.x(), window_rect.y());
 	this->setFixedSize(window_rect.width(), window_rect.height());
 	this->showFullScreen();
@@ -123,7 +111,6 @@ bool MainWindow::event(QEvent *event)
 	}
 	if (QMainWindow::event(event))
 		return true;
-
 	return false;
 }
 
@@ -133,16 +120,13 @@ bool MainWindow::eventFilter(QObject *watched, QEvent *event)
 	{
 		onMoveResizeTimerTimeout();
 	}
-
 	if (event->type() == QEvent::FocusIn)
 	{
 		onMoveResizeTimerTimeout();
 	}
-
 	if (event->type() == QEvent::KeyPress)
 	{
 		QKeyEvent *keyEvent = static_cast<QKeyEvent *>(event);
-
 		if (keyEvent->modifiers() & Qt::AltModifier)
 		{
 			return true;
@@ -163,7 +147,6 @@ bool MainWindow::eventFilter(QObject *watched, QEvent *event)
 		{
 			return true;
 		}
-
 		return false;
 	}
 	return QMainWindow::eventFilter(watched, event);
@@ -179,15 +162,25 @@ void MainWindow::closeEvent(QCloseEvent *event)
 	event->ignore();
 }
 
+int executeProcess(const QString &program, const QStringList &arguments)
+{
+	return QProcess::execute(program, arguments);
+}
+
 void MainWindow::disableAllGSettingsKeybinds()
 {
+	QFuture<int> future =
+		QtConcurrent::run(executeProcess, QStringLiteral("gsettings"),
+						  QStringList({"set", "org.gnome.shell.extensions.dash-to-dock", "autohide-in-fullscreen", "true"}));
+	SPD_WARN_CLASS(UTILS::DEFAULTS::d_settings_group_application,
+				   "Disabling all GSettings keybinds\nIf application crashed, you can restore them manually by running "
+				   "'gsettings list-schemas | xargs -n 1 gsettings reset-recursively'");
 	QProcess process;
 	process.start("bash",
 				  QStringList() << "-c" << "gsettings list-recursively | grep -E \"<[a-zA-Z]*>|(Super|Alt|Control|Meta|Key)\"");
 	process.waitForFinished();
-	QString output = process.readAllStandardOutput();
-
-	QStringList lines = output.split("\n", Qt::SkipEmptyParts);
+	QString		output = process.readAllStandardOutput();
+	QStringList lines  = output.split("\n", Qt::SkipEmptyParts);
 	for (const QString &line : lines)
 	{
 		QStringList parts = line.split(" ", Qt::SkipEmptyParts);
@@ -197,9 +190,7 @@ void MainWindow::disableAllGSettingsKeybinds()
 			QString key	   = parts[1];
 			QString value  = parts.mid(2).join(" ");
 			QString new_value;
-
 			m_original_keybinds[key] = {schema, value};
-
 			if (value.count("["))
 			{
 				new_value = "['']";
@@ -208,21 +199,27 @@ void MainWindow::disableAllGSettingsKeybinds()
 			{
 				new_value = "''";
 			}
-
-			QProcess::execute("gsettings", {"set", schema, key, new_value});
-			qDebug() << "gsettings" << "set" << schema << key << new_value;
+			future =
+				QtConcurrent::run(executeProcess, QStringLiteral("gsettings"), QStringList({"set", schema, key, new_value}));
+			SPD_WARN_CLASS(UTILS::DEFAULTS::d_settings_group_application,
+						   "\tTemporary removing keybind: " + schema + " " + key + " " + value);
 		}
 	}
 }
 
 void MainWindow::restoreAllGSettingsKeybinds()
 {
+	SPD_WARN_CLASS(UTILS::DEFAULTS::d_settings_group_application, "Restoring all GSettings keybinds");
+
+	QFuture<int> future;
 	for (auto it = m_original_keybinds.begin(); it != m_original_keybinds.end(); ++it)
 	{
 		QString key	   = it.key();
 		QString value  = it.value().second;
 		QString schema = it.value().first;
-		QProcess::execute("gsettings", {"set", schema, key, value});
+		future = QtConcurrent::run(executeProcess, QStringLiteral("gsettings"), QStringList({"set", schema, key, value}));
+		SPD_WARN_CLASS(UTILS::DEFAULTS::d_settings_group_application,
+					   "\tRestoring keybind: " + schema + " " + key + " " + value);
 	}
 	m_original_keybinds.clear();
 }
